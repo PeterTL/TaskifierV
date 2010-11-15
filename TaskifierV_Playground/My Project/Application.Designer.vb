8a6e30761d151daeb856e34a828c795734f08971
PeterTL <info@peterlangheinrich.de>
2010-11-15 09:42:28 +0100

    Added class structure, renamed main class.

--- TaskifierV_Playground/My Project/Application.Designer.vb
+++ TaskifierV_Playground/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.TaskifierV_Playground.Form1
+            Me.MainForm = Global.TaskifierV_Playground.TaskifierV
         End Sub
     End Class
 End Namespace
