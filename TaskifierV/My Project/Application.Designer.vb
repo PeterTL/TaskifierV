1fb0a21ecdf23872b693bf32c8d8ad1dfd4ff9a9
unknown <Peter@.(none)>
2010-11-17 11:36:36 +0100

    Mocking up some user interface. Created a form for that.

--- TaskifierV/My Project/Application.Designer.vb
+++ TaskifierV/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.TaskifierV.TaskifierV
+            Me.MainForm = Global.TaskifierV.frmUiMockup
         End Sub
     End Class
 End Namespace
