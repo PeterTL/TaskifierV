8b91c579d1909a4af334468974b65ccc12d20c5b
PeterTL <info@peterlangheinrich.de>
2010-12-06 14:51:55 +0100

    Added a new class for application graphic stuff. Maybe it's deprecated before used the first time but still...

---
+++ TaskifierV/Classes/AppGraphics.vb
@@ -0,0 +1,79 @@
+﻿Public Class AppGraphics
+
+    'TODO: CreateTagBox: Method documentation
+    Public Function CreateTagBox(ByRef loStrTags As List(Of String)) As TagTextBox
+        'Step 1: Create TagTextBox
+        Dim tb As New TagTextBox
+        'Step 2: Create TagLabels
+        Dim tl As New TagLabel
+        Dim position As Integer = 0
+        For Each strTag As String In loStrTags
+            tl = tl.CreateTagLabel(strTag, position)
+            tb.Controls.Add(tl)
+            position += tl.Width + 4
+        Next
+        tb.Width = position + 4
+        Return tb
+    End Function
+
+End Class
+
+Public Class TagTextBox
+    Inherits TextBox
+
+    Protected Overrides Sub OnPaint(ByVal e As System.Windows.Forms.PaintEventArgs)
+        MyBase.OnPaint(e)
+
+        ControlPaint.DrawBorder(e.Graphics, e.ClipRectangle, Color.LightGray, 1, _
+        ButtonBorderStyle.Solid, Color.LightGray, 1, ButtonBorderStyle.Solid, Color.LightGray, 1, _
+        ButtonBorderStyle.Solid, Color.LightGray, 1, ButtonBorderStyle.Solid)
+    End Sub
+
+    Public Sub New()
+        InitializeComponent()
+    End Sub
+
+    Private Sub InitializeComponent()
+        Me.Multiline = True
+        Me.Height = 24
+        'Me.Width = 300
+        Me.Padding = New Padding(0, 0, 0, 0)
+        Me.Margin = New Padding(0, 0, 0, 0)
+    End Sub
+
+End Class
+
+Public Class TagLabel
+    Inherits Label
+
+    Protected Overrides Sub OnPaint(ByVal e As System.Windows.Forms.PaintEventArgs)
+        MyBase.OnPaint(e)
+
+        ControlPaint.DrawBorder(e.Graphics, e.ClipRectangle, Color.LightGray, 1, _
+        ButtonBorderStyle.Solid, Color.LightGray, 1, ButtonBorderStyle.Solid, Color.LightGray, 1, _
+        ButtonBorderStyle.Solid, Color.LightGray, 1, ButtonBorderStyle.Solid)
+    End Sub
+
+    Public Function CreateTagLabel(ByRef strCaption As String, ByVal iPosX As Integer) As TagLabel
+        Dim lbl As New TagLabel
+        lbl.Height = 16
+        lbl.Width = strCaption.Length * 8
+        lbl.TextAlign = ContentAlignment.MiddleCenter
+        lbl.BackColor = Color.AliceBlue
+        lbl.Font = New Font("Lucida Console", 7, FontStyle.Regular, GraphicsUnit.Point, CType(0, Byte))
+        lbl.Text = strCaption
+        'lbl.Padding = New Padding(0, 0, 0, 0)
+        'lbl.Margin = New Padding(0, 0, 0, 0)
+        'lbl.Location = New Point(strCaption.Length * 8 + 2, 0)
+        lbl.Location = New Point(iPosX + 2, 2)
+        lbl.Cursor = Cursors.Hand
+        AddHandler lbl.MouseClick, AddressOf TagLabel_MouseClick
+
+        Return (lbl)
+    End Function
+
+    Private Sub TagLabel_MouseClick()
+        MsgBox("I was clicked.")
+    End Sub
+
+End Class
--- TaskifierV/Classes/TagAndTaskControl.vb
+++ TaskifierV/Classes/TagAndTaskControl.vb
@@ -2,22 +2,6 @@
 
 Public Class TagAndTaskControl
 
-    'TODO: CreateTagBox: Method documentation
-    Public Function CreateTagBox(ByRef loStrTags As List(Of String)) As TagTextBox
-        'Step 1: Create TagTextBox
-        Dim tb As New TagTextBox
-        'Step 2: Create TagLabels
-        Dim tl As New TagLabel
-        Dim position As Integer = 0
-        For Each strTag As String In loStrTags
-            tl = tl.CreateTagLabel(strTag, position)
-            tb.Controls.Add(tl)
-            position += tl.Width + 4
-        Next
-        tb.Width = position + 4
-        Return tb
-    End Function
-
     ''' <summary>
     ''' Returns a table with tags and their identifiers from the DB.
     ''' </summary>
@@ -41,10 +25,11 @@ Public Class TagAndTaskControl
 
         'Create row and DB objects
         Dim dr As DataRow
+        Dim v
         Dim DB As New TaskifierDB("Data/TaskifierDB.sdf")
 
         'Query DB for tags, filter is log type
-        Dim v = (From t In DB.Tags
+        v = (From t In DB.Tags
                        Join lett In DB.LogEntriesToTags
                        On lett.TagId Equals t.Id
                        Join le In DB.LogEntries
@@ -103,10 +88,10 @@ Public Class TagAndTaskControl
 
         'Create row and DB objects
         Dim dr As DataRow
+        Dim v
         Dim DB As New TaskifierDB("Data/TaskifierDB.sdf")
 
         'Check if tag identifier is set
-        Dim v
         If iTagId > -1 Then
             'Query DB for tasks, filter is tag and log type
             v = (From le In DB.LogEntries
@@ -156,6 +141,7 @@ Public Class TagAndTaskControl
         Return dt
     End Function
 
+    'TODO: Method documentation
     Public Function GetTaskDetails(ByVal iTaskId As Integer) As DataTable
         'Debug method parameters
         Debug.Print("")
@@ -169,62 +155,4 @@ Public Class TagAndTaskControl
         Return Nothing
     End Function
 
-End Class
-
-Public Class TagTextBox
-    Inherits TextBox
-
-    Protected Overrides Sub OnPaint(ByVal e As System.Windows.Forms.PaintEventArgs)
-        MyBase.OnPaint(e)
-
-        ControlPaint.DrawBorder(e.Graphics, e.ClipRectangle, Color.LightGray, 1, _
-        ButtonBorderStyle.Solid, Color.LightGray, 1, ButtonBorderStyle.Solid, Color.LightGray, 1, _
-        ButtonBorderStyle.Solid, Color.LightGray, 1, ButtonBorderStyle.Solid)
-    End Sub
-
-    Public Sub New()
-        InitializeComponent()
-    End Sub
-
-    Private Sub InitializeComponent()
-        Me.Multiline = True
-        Me.Height = 24
-        'Me.Width = 300
-        Me.Padding = New Padding(0, 0, 0, 0)
-        Me.Margin = New Padding(0, 0, 0, 0)
-    End Sub
-End Class
-
-Public Class TagLabel
-    Inherits Label
-
-    Protected Overrides Sub OnPaint(ByVal e As System.Windows.Forms.PaintEventArgs)
-        MyBase.OnPaint(e)
-
-        ControlPaint.DrawBorder(e.Graphics, e.ClipRectangle, Color.LightGray, 1, _
-        ButtonBorderStyle.Solid, Color.LightGray, 1, ButtonBorderStyle.Solid, Color.LightGray, 1, _
-        ButtonBorderStyle.Solid, Color.LightGray, 1, ButtonBorderStyle.Solid)
-    End Sub
-
-    Public Function CreateTagLabel(ByRef strCaption As String, ByVal iPosX As Integer) As TagLabel
-        Dim lbl As New TagLabel
-        lbl.Height = 16
-        lbl.Width = strCaption.Length * 8
-        lbl.TextAlign = ContentAlignment.MiddleCenter
-        lbl.BackColor = Color.AliceBlue
-        lbl.Font = New Font("Lucida Console", 7, FontStyle.Regular, GraphicsUnit.Point, CType(0, Byte))
-        lbl.Text = strCaption
-        'lbl.Padding = New Padding(0, 0, 0, 0)
-        'lbl.Margin = New Padding(0, 0, 0, 0)
-        'lbl.Location = New Point(strCaption.Length * 8 + 2, 0)
-        lbl.Location = New Point(iPosX + 2, 2)
-        lbl.Cursor = Cursors.Hand
-        AddHandler lbl.MouseClick, AddressOf TagLabel_MouseClick
-
-        Return (lbl)
-    End Function
-
-    Private Sub TagLabel_MouseClick()
-        MsgBox("I was clicked.")
-    End Sub
 End Class
